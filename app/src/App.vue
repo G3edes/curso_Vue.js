b9bd3c3434bfdd785fd4bb9f6abee83314063abf
GABRIEL SILVA GUEDES <24122379@dsn127m1242313.cfp127aluno.local>
2025-08-21 15:58:05 -0300

    aula 9

--- app/src/App.vue
+++ app/src/App.vue
@@ -18,13 +18,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 import AppHeader from './components/AppHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     AppHeader
   },
   data(){
--- app/src/components/AppHeader.vue
+++ app/src/components/AppHeader.vue
@@ -2,9 +2,43 @@
   <header class="header">
     oi
   </header>
-  <div v-for="local in todos" :key="local.id">
+
+
+
+  <!--
+  <div :class="{'title':false, 'title-home': ishome}"> TITULO </div>
+  <p class="['text', 'title]" > SDASDFSADFADISFJGIADUFIUSAFJIPUSDFJPIUSHFVSUHCPSIUHVNPSIUVHNSPIFUVBDIPVYBASPIVUCSDPVIUSVPIUSNVUIJ</p>
+  
+  <div class="todos" v-for="local in todos" v-bind:key="local.id">
+    <img v-if="local.Vimg" v-bind:src="local.Vimg" v-bind:alt="local.imgAlt">
+
     {{ local.title }}
+    
   </div>
+  -->
+  <div>
+    <input v-model="name" type="text">
+        {{name}}
+  </div>
+  <input
+      v-model="colors"
+      type="checkbox"
+      value="Amarelo"
+    />
+    Amarelo <br>
+
+    <input
+      v-model="colors"
+      type="checkbox"
+      value="Azul"
+    />
+    Azul <br>
+
+    <!-- Mostra os valores selecionados -->
+    Selecionado: {{ colors }}
+  
+
+
 </template>
 
 <script>
@@ -12,17 +46,30 @@ export default {
   name: 'AppHeader',  // vírgula aqui
   data() {
     return {
+
+      name: 'joan snow',
+      colors:[]
+  
+
+
+      /*pClass:"text",
+      ishome: true,
+      classVar:'title',
       todos: [
         {
           "userId": 1,
           "id": 1,
           "title": "delectus aut autem",
+          "Vimg":'https://placehold.co/600x400',
+          "imgAlt":"foto john snow",
           "completed": false
+          
         },
         {
           "userId": 1,
           "id": 2,
           "title": "quis ut nam facilis et officia qui",
+          "Vimg":'https://placehold.co/600x400',
           "completed": false
         },
         {
@@ -37,13 +84,24 @@ export default {
           "title": "et porro tempora",
           "completed": true
         }
-      ]
+      ]*/
     }
   }
 }
 </script>
 
 <style>
+.text{
+  color:purple;
+}
+.title-home{
+  color:green;
+}
+.title{
+  color:red;
+  font-size:200px;
+  padding:1rem;
+}
 .header {
   background-color: #000;
   color: #fff;
@@ -51,6 +109,10 @@ export default {
 </style>
 
 <style>
+.todos{
+  background-color: #000;
+  color: white;
+}
 .header {
   background-color: #000;
   color: #fff;
