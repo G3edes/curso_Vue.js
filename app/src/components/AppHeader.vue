96e735a1aeffbd4a55deff9ae77b607e64265e21
GABRIEL SILVA GUEDES <24122379@dsn127m1242313.cfp127aluno.local>
2025-08-21 17:11:57 -0300

    aula 10

--- app/src/components/AppHeader.vue
+++ app/src/components/AppHeader.vue
@@ -2,43 +2,6 @@
   <header class="header">
     oi
   </header>
-
-
-
-  <!--
-  <div :class="{'title':false, 'title-home': ishome}"> TITULO </div>
-  <p class="['text', 'title]" > SDASDFSADFADISFJGIADUFIUSAFJIPUSDFJPIUSHFVSUHCPSIUHVNPSIUVHNSPIFUVBDIPVYBASPIVUCSDPVIUSVPIUSNVUIJ</p>
-  
-  <div class="todos" v-for="local in todos" v-bind:key="local.id">
-    <img v-if="local.Vimg" v-bind:src="local.Vimg" v-bind:alt="local.imgAlt">
-
-    {{ local.title }}
-    
-  </div>
-  -->
-  <div>
-    <input v-model="name" type="text">
-        {{name}}
-  </div>
-  <input
-      v-model="colors"
-      type="checkbox"
-      value="Amarelo"
-    />
-    Amarelo <br>
-
-    <input
-      v-model="colors"
-      type="checkbox"
-      value="Azul"
-    />
-    Azul <br>
-
-    <!-- Mostra os valores selecionados -->
-    Selecionado: {{ colors }}
-  
-
-
 </template>
 
 <script>
@@ -46,46 +9,13 @@ export default {
   name: 'AppHeader',  // vírgula aqui
   data() {
     return {
-
-      name: 'joan snow',
-      colors:[]
-  
-
-
-      /*pClass:"text",
-      ishome: true,
-      classVar:'title',
-      todos: [
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "Vimg":'https://placehold.co/600x400',
-          "imgAlt":"foto john snow",
-          "completed": false
-          
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "Vimg":'https://placehold.co/600x400',
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-        }
-      ]*/
     }
+  }
+  method() {
+    onClick(){
+      
+    }
+    
   }
 }
 </script>
